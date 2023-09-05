bfaed4d8512a8e4be206620c46ee339889a9bba4
MonoCalvo <126566819+MonoCalvo@users.noreply.github.com>
2023-09-05 12:23:49 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,9 @@
+
+//-------------------------------------------
+//daqui pea começa o js do site
+// busca no site pelo titulo e armazena 
+let titulo = document.querySelector("h1");
+// mostra no devoolts o conteudo
+console.log(titulo.textContent);
+titulo.textContent = fica grande nutricaos
+//altera o conteudon da variavel tiulo 
